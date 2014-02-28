70588667bdd8a2c5667b629a86295de4fe686490
RushadHeerjee <rushad_heerjee@hotmail.com>
2014-02-27 22:00:27 -0500

    Update CrosswalkH.java
    Added additional comments

--- EvacuationSimulator/src/evacSim/core/CrosswalkH.java
+++ EvacuationSimulator/src/evacSim/core/CrosswalkH.java
@@ -1,14 +1,23 @@
 package evacSim.core;
-
+/**
+ * Determines the attributes to cross a horizontally facing crosswalk
+ * 
+ * @author Daniel Keyes
+ * 
+ */ 
 public class CrosswalkH extends Crosswalk {
 
 	public CrosswalkH() {
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	/**
+	 * Determine if the horizontal crosswalk is walkable 
+	 */
 	@Override
 	boolean isWalkable(Cell walker) {
 		if(!(walker instanceof Person)){
+			//If the walker is not a person
 			return false;
 		}
 		Person pWalker = (Person) walker;
